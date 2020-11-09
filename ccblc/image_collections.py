8802b5802a20fc18e6be6a0596852ef2c73b1468
earth-chris <cbanderson08@gmail.com>
2020-11-09 14:58:21 -0800

    code formatting

--- ccblc/image_collections.py
+++ ccblc/image_collections.py
@@ -1,24 +1,28 @@
 """Functions to return the default image collections for linear unmixing and surface temp retrieval
 """
 import sys as _sys
-if 'ee' not in sys.modules:
+
+if "ee" not in sys.modules:
     import ee
+
     ee.Initialize()
 
 # landsat 5
 def landsat_5():
     bnds = []
-    coll = ee.ImageCollection('LANDSAT/LT05/C01/T2_SR')
+    coll = ee.ImageCollection("LANDSAT/LT05/C01/T2_SR")
     return bnds, coll
-    
+
+
 # landsat 7
 def landsat_7():
     bnds = []
-    coll = ee.ImageCollection('LANDSAT/LE07/C01/T2_SR')
+    coll = ee.ImageCollection("LANDSAT/LE07/C01/T2_SR")
     return bnds, coll
-    
+
+
 # landsat 8
 def landsat_8():
-    bnds = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7']
-    coll = ee.ImageCollection('LANDSAT/LC08/C01/T2_SR')
+    bnds = ["B2", "B3", "B4", "B5", "B6", "B7"]
+    coll = ee.ImageCollection("LANDSAT/LC08/C01/T2_SR")
     return bnds, coll
