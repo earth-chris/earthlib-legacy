651552c27caedb7a5910c185e18f8c8f0db302cb
earth-chris <cbanderson08@gmail.com>
2020-11-09 15:13:42 -0800

    reformatted docstrings, updated checkFile routine

--- ccblc/Read.py
+++ ccblc/Read.py
@@ -4,20 +4,18 @@ Functions for reading specifically formatted data
 import numpy as np
 import spectral
 
-from ccblc.utils import spectralObject
+from ccblc.utils import spectralObject, checkFile
 
 
 # function to read ascii spectra from the joint fire science program
 # (https://www.frames.gov/partner-sites/assessing-burn-severity/spectral/spectral-library-southern-california/)
-def jfsc(path):
-    """reads the ascii format spectral data from the joint-fire-science-program
+def jfsp(path):
+    """
+    Reads the ASCII format spectral data from the joint-fire-science-program
     and returns an object with the mean and +/- standard deviation reflectance data
 
-    Args:
-        infile:
-
-    Returns:
-        a ccblc spectralObject with the jfsp reflectance data
+    :param path: file path to the JFSP spectra text file
+    :return s: a ccblc spectralObject with the JFSP reflectance data
     """
 
     # create the spectral object
@@ -28,13 +26,11 @@ def jfsc(path):
     # open the file and read the data
     with open(path, "r") as f:
         header = f.readline()
-        i = 0
-        for line in f:
+        for i, line in enumerate(f):
             line = line.strip().split()
             s.spectra[0, i] = line[1]
             s.spectra_stdevp[0, i] = line[2]
             s.spectra_stdevm[0, i] = line[3]
-            i += 1
 
         # return the spectral object
         return s
@@ -42,25 +38,23 @@ def jfsc(path):
 
 # function to read an envi spectral library file
 def spectralLibrary(path):
-    """reads an envi format spectral library
-
-    Args:
-        infile: the envi spectral library file (looks for an accompanying .hdr)
-
-    Returns:
-        a ccblc spectralObject with the spectral library data
+    """
+    Reads an ENVI-format spectral library into memory.
+    
+    :param path: file path to the ENVI spectral library file. Looks for an accompanying .hdr file.
+    :return s: a ccblc spectralObject with the spectral library data
     """
     # check for header files
-    if check_file(infile[:-4] + ".hdr", quiet=True):
-        hdr = infile[:-4] + ".hdr"
+    if checkFile(path[:-4] + ".hdr"):
+        hdr = path[:-4] + ".hdr"
     else:
-        if check_file(infile + ".hdr"):
-            hdr = infile + ".hdr"
+        if checkFile(path + ".hdr"):
+            hdr = path + ".hdr"
         else:
             return None
 
     # read the spectral data
-    slib = spectral.envi.open(hdr, infile)
+    slib = spectral.envi.open(hdr, path)
 
     # create the spectral object
     s = spectralObject(
@@ -83,3 +77,4 @@ def spectralLibrary(path):
 del np
 del spectral
 del spectralObject
+del checkFile
