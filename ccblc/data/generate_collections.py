a696773bacfd4bb41a0f496f466da0996f012763
earth-chris <cbanderson08@gmail.com>
2020-11-09 16:10:46 -0800

    init

---
+++ ccblc/data/generate_collections.py
@@ -0,0 +1,34 @@
+"""
+A script to create a formatted json file with sensor/collection parameters
+"""
+import json
+
+
+collections = {
+    # from http://www.gisagmaps.com/landsat-8-sentinel-2-bands/
+    "Landsat8": {
+        "collection": "LANDSAT/LC08/C01/T1_SR",
+        "band_names": [
+            "B1",
+            "B2",
+            "B3",
+            "B4",
+            "B5",
+            "B6",
+            "B7",
+            "B10",
+            "B11",
+        ],
+        "band_descriptions": [
+            "ultra blue",
+            "blue",
+            "green",
+            "red",
+            "near infrared",
+            "shortwave infrared 1",
+            "shortwave infrared 2",
+        ],
+        "band_centers": [0.443, 0.482, 0.561, 0.655, 0.865, 1.609, 2.201],
+        "band_widths": [0.016, 0.060, 0.057, 0.037, 0.028, 0.085, 0.187],
+    }
+}
