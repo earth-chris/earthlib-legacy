d9db55edcf46df00e819ff8fd14a35c541a2c338
earth-chris <cbanderson08@gmail.com>
2020-11-10 16:37:09 -0800

    updated s2 to cloud and cirrus

--- ccblc/Mask.py
+++ ccblc/Mask.py
@@ -18,12 +18,8 @@ def Landsat8(img):
 def Sentinel2(img):
     import ee
 
-    # get the cloud mask bit corresponding to the qa band
-    cloud_bit = 10
-    cloud_bit_mask = 2 ** cloud_bit
-
-    # label the cloudy pixels
-    mask = img.select("QA60").bitwiseAnd(cloud_bit_mask).eq(0)
-
-    # and apply the mask to the image
+    cirrusBitMask = ee.Number(2).pow(11).int()
+    cloudsBitMask = ee.Number(2).pow(10).int()
+    qa = img.select("QA60")
+    mask = qa.bitwiseAnd(cirrusBitMask).eq(0).And(qa.bitwiseAnd(cloudsBitMask).eq(0))
     return img.mask(mask)
