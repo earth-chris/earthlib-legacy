53b59c012a5d9cf3f39e09b99173ea3bd8bb90b2
earth-chris <cbanderson08@gmail.com>
2020-11-10 16:26:07 -0800

    pythonized the l8 routine

--- ccblc/Mask.py
+++ ccblc/Mask.py
@@ -3,30 +3,27 @@ Functions for masking earth engine images
 """
 
 
-class Clouds:
-    def __init__(self):
-        pass
+def Landsat8(img):
+    import ee
 
-    @staticmethod
-    def Landsat8(img):
-        # get the cloud mask bit corresponding to the qa band
-        cloud_bit = 1
-        cloud_bit_mask = 2 ** cloud_bit
+    cloudShadowBitMask = ee.Number(2).pow(3).int()
+    cloudsBitMask = ee.Number(2).pow(5).int()
+    qa = img.select("pixel_qa")
+    mask = (
+        qa.bitwiseAnd(cloudShadowBitMask).eq(0).And(qa.bitwiseAnd(cloudsBitMask).eq(0))
+    )
+    return img.mask(mask)
 
-        # label the cloudy pixels a la the selected bits
-        mask = img.select("pixel_qa").bitwiseAnd(cloud_bit_mask).eq(0)
 
-        # and apply the mask to the image
-        return img.mask(mask)
+def Sentinel2(img):
+    import ee
 
-    @staticmethod
-    def Sentinel2(img):
-        # get the cloud mask bit corresponding to the qa band
-        cloud_bit = 10
-        cloud_bit_mask = 2 ** cloud_bit
+    # get the cloud mask bit corresponding to the qa band
+    cloud_bit = 10
+    cloud_bit_mask = 2 ** cloud_bit
 
-        # label the cloudy pixels
-        mask = img.select("QA60").bitwiseAnd(cloud_bit_mask).eq(0)
+    # label the cloudy pixels
+    mask = img.select("QA60").bitwiseAnd(cloud_bit_mask).eq(0)
 
-        # and apply the mask to the image
-        return img.mask(mask)
+    # and apply the mask to the image
+    return img.mask(mask)
