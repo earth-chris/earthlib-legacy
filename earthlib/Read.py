6736ee88e030babc97bb68be2cf36481cc4716fb
Christopher Anderson <cbanders@stanford.edu>
2021-05-22 23:47:26 -0700

    docstring update

--- earthlib/Read.py
+++ earthlib/Read.py
@@ -1,18 +1,22 @@
 """Functions for reading specifically formatted data."""
 
+import numpy as _np
+import spectral as _spectral
+
+from earthlib.utils import _endmember_path
+from earthlib.utils import spectralObject as _spectralObject
+
 
 # for ENVI spectral libraries
 def spectralLibrary(path):
-    """
-    Reads an ENVI-format spectral library into memory.
+    """Reads an ENVI-format spectral library into memory.
 
-    :param path: file path to the ENVI spectral library file. Looks for an accompanying .hdr file.
-    :return s: an earthlib spectralObject with the spectral library data
-    """
-    import numpy as np
-    import spectral
+    Args:
+        path: file path to the ENVI spectral library file. Looks for a .hdr sidecar file.
 
-    from earthlib.utils import checkFile, spectralObject
+    Returns:
+        s: an earthlib spectralObject with the spectral library data.
+    """
 
     # check for header files
     if checkFile(path[:-4] + ".hdr"):
@@ -24,54 +28,55 @@ def spectralLibrary(path):
             return None
 
     # read the spectral data
-    slib = spectral.envi.open(hdr, path)
-
-    # create the spectral object
-    s = spectralObject(
+    slib = _spectral.envi.open(hdr, path)
+    s = _spectralObject(
         slib.params.nrows,
         slib.params.ncols,
-        band_centers=np.asarray(slib.bands.centers),
+        band_centers=_np.asarray(slib.bands.centers),
         band_unit=slib.bands.band_unit,
         band_quantity=slib.bands.band_quantity,
     )
 
-    # set the spectra and names
+    # pull the spectra and names from the library
     s.spectra = slib.spectra
     s.names = slib.names
 
-    # return the final object
     return s
 
 
 def endmembers():
-    """
-    Reads the earthlib spectral endmember library
+    """Reads the earthlib spectral endmember library.
 
-    :return s: an earthlib spectralObject with the endmember library reflectance data
+    Args:
+        None
+
+    Returns:
+        s: an earthlib spectralObject with the endmember library reflectance data
     """
-    from .utils import _endmember_path
 
     s = spectralLibrary(_endmember_path)
+
     return s
 
 
-# ascii spectra from the joint fire science program (https://www.frames.gov/assessing-burn-severity/spectral-library/overview)
 def jfsp(path):
-    """
+    """Reads JFSP-formatted ASCII files.
+
     Reads the ASCII format spectral data from the joint-fire-science-program
-    and returns an object with the mean and +/- standard deviation reflectance data
+    and returns an object with the mean and +/- standard deviation reflectance data.
+    Reference: https://www.frames.gov/assessing-burn-severity/spectral-library/overview
 
-    :param path: file path to the JFSP spectra text file
-    :return s: an earthlib spectralObject with the JFSP reflectance data
+    Args:
+        path: file path to the JFSP spectra text file
+
+    Returns:
+        s: an earthlib spectralObject with the JFSP reflectance data.
     """
-    import numpy as np
-
-    from earthlib.utils import spectralObject
 
     # create the spectral object
     s = spectralObject(1, type="asd")
-    s.spectra_stdevm = np.zeros(s.spectra.shape)
-    s.spectra_stdevp = np.zeros(s.spectra.shape)
+    s.spectra_stdevm = _np.zeros(s.spectra.shape)
+    s.spectra_stdevp = _np.zeros(s.spectra.shape)
 
     # open the file and read the data
     with open(path, "r") as f:
@@ -82,21 +87,21 @@ def jfsp(path):
             s.spectra_stdevp[0, i] = line[2]
             s.spectra_stdevm[0, i] = line[3]
 
-        # return the spectral object
         return s
 
 
-# ascii spetra from the USGS (https://www.sciencebase.gov/catalog/item/5807a2a2e4b0841e59e3a18d)
 def usgs(path):
-    """
-    Reads the ascii format spectral data from USGS and returns an object with the mean and +/- standard deviation
+    """Reads USGS-formatted ASCII files.
 
-    :param path: file path the the USGS spectra text file
-    :return s: an earthlib spectralObject with the USGS reflectance data
-    """
-    import numpy as np
+    Reads the ascii format spectral data from USGS and returns an object with the mean
+    and +/- standard deviation. Reference: https://www.sciencebase.gov/catalog/item/5807a2a2e4b0841e59e3a18d
 
-    from earthlib.utils import spectralObject
+    Args:
+        path: file path the the USGS spectra text file
+
+    Returns:
+        s: an earthlib spectralObject with the USGS reflectance data.
+    """
 
     # open the file and read header info
     with open(path, "r") as f:
@@ -118,9 +123,8 @@ def usgs(path):
                 n_values = int(line.strip().split()[-1])
 
         # now that we got our header info, create the arrays
-        #  necessary for output
-        band_centers = np.empty(n_values)
-        reflectance = np.empty(n_values)
+        band_centers = _np.empty(n_values)
+        reflectance = _np.empty(n_values)
 
         line = line.strip().split()
         band_centers[0] = float(line[0])
@@ -134,7 +138,7 @@ def usgs(path):
             reflectance[i] = float(line[1])
             i += 1
 
-        # some files read last -> first wavelength. resort as necessary
+        # some files read last -> first wavelength
         if band_centers[0] > band_centers[-1]:
             band_centers = band_centers[::-1]
             reflectance = reflectance[::1]
@@ -143,6 +147,7 @@ def usgs(path):
         if band_unit.lower() == "micrometers":
             band_centers *= 1000.0
             band_unit = "Nanometers"
+
         if refl_unit.lower() == "percent":
             reflectance /= 100.0
 
@@ -160,5 +165,4 @@ def usgs(path):
         if spectrum_name:
             s.names[0] = spectrum_name
 
-    # return the final object
     return s
