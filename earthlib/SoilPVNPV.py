6135d343ec25ba1ace9a4c59f426725ce54cc1ba
earth-chris <cba@salo.ai>
2023-09-10 11:41:04 -0700

    unmixing class init

--- earthlib/SoilPVNPV.py
+++ earthlib/SoilPVNPV.py
@@ -22,7 +22,19 @@ def bySensor(sensor: str) -> Callable:
         the scale function associated with a sensor to pass to an ee .map() call
     """
     lookup = {
+        "ASTER": ASTER,
+        "AVNIR2": AVNIR2,
+        "DoveR": DoveR,
+        "Landsat4": Landsat457,
+        "Landsat5": Landsat457,
+        "Landsat7": Landsat457,
         "Landsat8": Landsat8,
+        "MODIS": MODIS,
+        "NEON": NEON,
+        "PlanetScope": PlanetScope,
+        "Sentinel2": Sentinel2,
+        "SuperDove": SuperDove,
+        "VIIRS": VIIRS,
     }
     try:
         function = lookup[sensor]
@@ -55,6 +67,134 @@ def getEndmembers(sensor: str, bands: list, n: int = N_ITERATIONS) -> Tuple[list
     return soil, pv, npv
 
 
+def ASTER(
+    img: ee.Image,
+    bands: list = getBands("ASTER"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix an ASTER image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "ASTER"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def AVNIR2(
+    img: ee.Image,
+    bands: list = getBands("AVNIR2"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix an AVNIR2 image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "AVNIR2"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def DoveR(
+    img: ee.Image,
+    bands: list = getBands("DoveR"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a Landsat8 image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "DoveR"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def Landsat457(
+    img: ee.Image,
+    bands: list = getBands("Landsat7"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a Landsat4 image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "Landsat7"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
 def Landsat8(
     img: ee.Image,
     bands: list = getBands("Landsat8"),
@@ -71,12 +211,201 @@ def Landsat8(
             reduces the influences of brightness and illumination geometry.
 
     Returns:
-        unmixed: a 4- or 5-band image with bands (%soil, %pv, %npv, RMSE) or
-            (%soil, %pv, %npv, %shade, RMSE) depending on whether shade
-            normalization is applied. The first three bands should sum to 1
-            regardless of whether normalization was applied.
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
     """
     sensor = "Landsat8"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def MODIS(
+    img: ee.Image,
+    bands: list = getBands("MODIS"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a MODIS image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "MODIS"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def NEON(
+    img: ee.Image,
+    bands: list = getBands("NEON"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a NEON image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "NEON"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def PlanetScope(
+    img: ee.Image,
+    bands: list = getBands("PlanetScope"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a PlanetScope image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "PlanetScope"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def Sentinel2(
+    img: ee.Image,
+    bands: list = getBands("Sentinel2"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a Sentinel2 image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "Sentinel2"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def SuperDove(
+    img: ee.Image,
+    bands: list = getBands("SuperDove"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a SuperDove image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "SuperDove"
+    n_bands = len(bands)
+    endmembers = getEndmembers(sensor, bands, n)
+    unmixed = fractionalCover(
+        img,
+        endmembers,
+        endmember_names=ENDMEMBER_NAMES,
+        shade_normalize=shade_normalize,
+        n_bands=n_bands,
+    )
+
+    return unmixed
+
+
+def VIIRS(
+    img: ee.Image,
+    bands: list = getBands("VIIRS"),
+    n: int = N_ITERATIONS,
+    shade_normalize: bool = SHADE_NORMALIZE,
+) -> ee.Image:
+    """Unmix a VIIRS image with soil, pv, npv endmembers.
+
+    Args:
+        image: the ee.Image to unmix
+        bands: a list of bands to select (from earthlib.getBands(sensor)).
+        n: the number of iterations for unmixing.
+        shade_normalize: apply shade normalization during unmixing.
+            reduces the influences of brightness and illumination geometry.
+
+    Returns:
+        unmixed: a 3-band image with bands (%soil, %pv, %npv).
+    """
+    sensor = "VIIRS"
     n_bands = len(bands)
     endmembers = getEndmembers(sensor, bands, n)
     unmixed = fractionalCover(
